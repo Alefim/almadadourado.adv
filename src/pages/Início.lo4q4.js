729079ec9e48b1730d5ce7e46eb6a8fece121c85
alefim.dev <tec.alefoliveira@gmail.com>
2025-04-23 22:49:28 -0300

    atualizar

--- src/pages/Início.lo4q4.js
+++ src/pages/Início.lo4q4.js
@@ -2,5 +2,12 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    
+    // Código básico Velo no Wix
+$w.onReady(function () {
+    // Animações simples ao carregar a página
+    $w("#heroSection").show("fade", { duration: 800 });
+    $w("#logoImage").show("fade", { duration: 1200 });
+    $w("#titleText").show("fade", { duration: 1400 });
+});
+
 });
